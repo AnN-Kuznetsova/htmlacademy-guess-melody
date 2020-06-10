08db4390cc330e75296772824a2579013754b5c9
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-06-10 23:37:31 +0300

    Добавит пропсы в компоненты.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
 
-const App = () => {
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {errorCount} = props;
+
   return (
-    <WelcomeScreen />
+    <WelcomeScreen errorCount={errorCount} />
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDom from "react-dom";
 import App from "./components/app/app.jsx";
 
 
+const settings = {
+  ERRORS_COUNT: 3,
+};
+
 ReactDom.render(
-    <App />,
+    <App errorCount={settings.ERRORS_COUNT} />,
     document.querySelector(`#root`)
 );
