d835caaa686917186d499ebfb68599884abecb39
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-06-14 03:39:07 +0300

    Добавляет проверку 'WelcomeScreen' на корректное отображение errorCount.

--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -1,17 +1,30 @@
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import React from "react";
 import renderer from "react-test-renderer";
 import {WelcomeScreen} from "./welcome-screen.jsx";
 
 
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+
+const props = {
+  errorCount: 3,
+  welcomeButtonClickHandler: () => {},
+};
+
+
 describe(`Render WelcomeScreen`, () => {
   it(`Render correctly WelcomeScreen component`, () => {
-    const props = {
-      errorCount: 3,
-      welcomeButtonClickHandler: () => {},
-    };
-
     const welcomeScreenComponent = renderer.create(<WelcomeScreen {...props} />).toJSON();
-
     expect(welcomeScreenComponent).toMatchSnapshot();
+  });
+
+  it(`Render correctly errors count`, () => {
+    const welcomeScreenComponent = shallow(<WelcomeScreen {...props} />);
+    expect(welcomeScreenComponent.find(`ul.welcome__rules-list li`).last().text())
+      .toEqual(`Можно допустить ${props.errorCount} ошибки.`);
   });
 });
