4f709df677cc99d5677d01e8ec57595cd1548a74
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-06-14 03:13:30 +0300

    Добавит e2e-тест для компонента 'WelcomeScreen'.

---
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import {WelcomeScreen} from "./welcome-screen.jsx";
+
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+
+describe(`WelcomeScreen e2e-tests`, () => {
+  it(`Should welcomeButton be pressed`, () => {
+    const welcomeButtonClickHandler = jest.fn();
+
+    const props = {
+      errorCount: 3,
+      welcomeButtonClickHandler,
+    };
+
+    const welcomeScreen = shallow(<WelcomeScreen {...props} />);
+    const welcomeButton = welcomeScreen.find(`button.welcome__button`);
+
+    welcomeButton.simulate(`click`);
+    expect(welcomeButtonClickHandler).toHaveBeenCalled();
+  });
+});
