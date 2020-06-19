d1b43c24457795367b5a6773eb834ad67598e5b5
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-06-19 11:44:19 +0300

    Исправит svg в 'ArtistQuestionScreen'.

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -16,10 +16,10 @@ export const ArtistQuestionScreen = (props) => {
         </a>
 
         <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}
+          />
         </svg>
-        <circle className="timer__line" cx="390" cy="390" r="370"
-          style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}
-        />
 
         <div className="game__mistakes">
           <div className="wrong"></div>
