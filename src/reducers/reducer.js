6232ea1b21c276c4e97430668ce2d1c4242de509
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-06 14:40:17 +0300

    Добавит 'reducer' и тесты к нему.

---
+++ src/reducers/reducer.js
@@ -0,0 +1,35 @@
+import {extend} from "../utils/utils.js";
+
+
+const initialState = {
+  mistakes: 0,
+  step: -1,
+};
+
+const ActionType = {
+  INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
+  INCREMENT_STEP: `INCREMENT_STEP`,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.INCREMENT_STEP:
+      return extend(state, {
+        step: state.step + action.payload,
+      });
+
+    case ActionType.INCREMENT_MISTAKES:
+      return extend(state, {
+        mistakes: state.mistakes + action.payload,
+      });
+
+    default:
+      return state;
+  }
+};
+
+
+export {
+  ActionType,
+  reducer,
+};
