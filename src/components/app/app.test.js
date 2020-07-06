26ba8ccd65df59d251d3a21ecd768a26636b68c7
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-06 16:01:17 +0300

    Обновит тесты для 'App'.

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -4,15 +4,48 @@ import {App} from "./app.jsx";
 import {MAX_ERRORS_COUNT, artistQuestion, genreQuestion} from "../../__test-data__/test-mocks.js";
 
 
+const nodeMock = {
+  createNodeMock: () => {
+    return {};
+  }
+};
+
 const props = {
   maxErrorsCount: MAX_ERRORS_COUNT,
   questions: [genreQuestion, artistQuestion],
+  step: null,
+  onWelcomeButtonClick: () => {},
+  onUserAnswer: () => {},
 };
 
 
 describe(`Render App`, () => {
-  it(`Should match with snapshot`, () => {
-    const welcomeScreenSnapshot = renderer.create(<App {...props} />).toJSON();
-    expect(welcomeScreenSnapshot).toMatchSnapshot();
+  it(`Render Welcome should match with snapshot`, () => {
+    props.step = -1;
+    const treeSnapshot = renderer.create(
+        <App {...props} />
+    ).toJSON();
+
+    expect(treeSnapshot).toMatchSnapshot();
+  });
+
+
+  it(`Render GuesGenreGame should match with snapshot`, () => {
+    props.step = 0;
+    const treeSnapshot = renderer.create(
+        <App {...props} />, nodeMock
+    ).toJSON();
+
+    expect(treeSnapshot).toMatchSnapshot();
+  });
+
+
+  it(`Render GuesArtistGame should match with snapshot`, () => {
+    props.step = 1;
+    const treeSnapshot = renderer.create(
+        <App {...props} />, nodeMock
+    ).toJSON();
+
+    expect(treeSnapshot).toMatchSnapshot();
   });
 });
