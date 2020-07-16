9a77aeeab5e1a97969d64e716e6370b975271182
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-16 03:26:48 +0300

    Исправит ошибку линтинга.

--- src/components/audio-player/audio-player.test.js
+++ src/components/audio-player/audio-player.test.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import {AudioPlayer} from './audio-player.jsx';
-import {mount} from 'enzyme';
 
-
-/* const song = {
-  src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
-}; */
 
 const props = {
   isLoading: true,
