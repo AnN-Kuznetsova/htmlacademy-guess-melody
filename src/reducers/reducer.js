d36486dc3a5aeeae94cb7e03d916240270bbfea8
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-20 15:26:22 +0300

    Объединит редьюсеры с помощью combineReducers и NameSpace.

---
+++ src/reducers/reducer.js
@@ -0,0 +1,13 @@
+import {combineReducers} from "redux";
+
+import {NameSpace} from "./name-space.js";
+import {reducer as data} from "./data/data.js";
+import {reducer as game} from "./game/game.js";
+import {reducer as user} from "./user/user.js";
+
+
+export const reducer = combineReducers({
+  [NameSpace.DATA]: data,
+  [NameSpace.GAME]: game,
+  [NameSpace.USER]: user,
+});
