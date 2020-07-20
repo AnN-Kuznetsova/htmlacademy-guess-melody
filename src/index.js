99635ec0fc69a503cd1c0c9e104f5eaec345f114
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-20 23:18:44 +0300

    Подключит декомпозированные редьюсеры и Thunk в приложение.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
-import {App} from "./components/app/app.jsx";
+import thunk from "redux-thunk";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+
+import {App} from "./components/app/app.jsx";
+import {Operation as DataOperation} from "./reducer/data/data.js";
+import {Operation as UserOperation, ActionCreator, AuthorizationStatus} from "./reducer/user/user.js";
+import {createAPI} from "./api.js";
 import {reducer} from "./reducers/reducer.js";
 
 
+const api = createAPI(() => {});
+
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    applyMiddleware(thunk.withExtraArgument(api))
 );
 
 
